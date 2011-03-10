a91a3af2a458ec6c559c57b41a8021e2ffc4f906
phuong_vu <phuong_vu@exoplatform.com>
2011-03-10 09:20:39 +0700

    Change BundleResolver to UserPortalContext in PortalRequestContext

--- webui/portal/src/main/java/org/exoplatform/portal/webui/portal/UIPortalForm.java
+++ webui/portal/src/main/java/org/exoplatform/portal/webui/portal/UIPortalForm.java
@@ -315,7 +315,7 @@ public class UIPortalForm extends UIFormTabPane
             {
                PortalConfig portalConfig = (PortalConfig)PortalDataMapper.buildModelObject(uiPortal);
                dataService.save(portalConfig);
-               prContext.setAttribute(UserPortalConfig.class, service.getUserPortalConfig(uiForm.getPortalOwner(), prContext.getRemoteUser(), prContext.getBundleResolver()));
+               prContext.setAttribute(UserPortalConfig.class, service.getUserPortalConfig(uiForm.getPortalOwner(), prContext.getRemoteUser(), prContext.getUserPortalContext()));
                uiPortalApp.reloadSkinPortal(prContext);
             }
             else
